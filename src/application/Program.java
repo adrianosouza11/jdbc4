fb28912491c37da75e5f68673b96389310152012
Adriano O. Souza <adrianosouza11@gmail.com>
2019-01-19 12:53:51 -0200

    JDBC Update

---
+++ src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement st =null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller " 
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffectd = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffectd);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
